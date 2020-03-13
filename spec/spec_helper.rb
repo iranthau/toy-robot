15af9694c25c54006fe81c9745be5ff7a05d0ca8
Iran R <iranthar@rent.com.au>
2020-03-14 03:34:19 +0800

    Updare env setup

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,16 @@
+# See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
+
 require 'toy_robot/robot'
 require 'toy_robot/position'
 require 'toy_robot/board'
+
+RSpec.configure do |config|
+  config.expect_with :rspec do |expectations|
+    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
+  end
+  config.mock_with :rspec do |mocks|
+    mocks.verify_partial_doubles = true
+  end
+
+  config.shared_context_metadata_behavior = :apply_to_host_groups
+end
