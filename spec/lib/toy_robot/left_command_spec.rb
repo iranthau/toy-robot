f690e00d64250449e0e39239a388be95822675b2
Iran R <iranthar@rent.com.au>
2020-03-15 07:39:46 +0800

    Change to command oriented design

---
+++ spec/lib/toy_robot/left_command_spec.rb
@@ -0,0 +1,34 @@
+RSpec.describe ToyRobot::LeftCommand do
+  subject(:left_command) { described_class.new(robot) }
+
+  let(:robot) { nil }
+
+  describe '.new' do
+    context 'when the robot is missing' do
+      it 'raises an error' do
+        expect{ left_command }.to raise_error(ToyRobot::CommandError, 'robot is required')
+      end
+    end
+
+    context 'when the params are given' do
+      let(:robot) { instance_double(ToyRobot::Robot) }
+
+      it 'does NOT rais an error' do
+        expect{ left_command }.not_to raise_error
+      end
+    end
+  end
+
+  describe '#run' do
+    subject(:run) { left_command.run }
+
+    let(:robot) { instance_double(ToyRobot::Robot, turn_left: true, report: true) }
+
+    it 'turns left' do
+      run
+
+      expect(robot).to have_received(:turn_left)
+      expect(robot).to have_received(:report)
+    end
+  end
+end
